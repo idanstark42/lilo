21f9724941c67f7c561b8c389d940dd088c89e2f
Idan Stark <idanstark42@gmail.com>
2025-01-15 22:01:53 +0100

    added logs

--- index.js
+++ index.js
@@ -37,6 +37,8 @@ async function handleAuth(authLevel, headerAuth) {
   if (!token) throw { status: 400, message: 'Invalid Authorization format' }
 
   try {
+    console.log('token')
+    console.log(token)
     const response = await stytch.oauth.authenticate(token)
     const userId = response.user_id
     console.log('userId')
