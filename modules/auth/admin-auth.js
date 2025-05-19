496cf3e7cbb59eb179f0f01fe6bdc29c5e2f02aa
Idan Stark <idanstark42@gmail.com>
2025-05-19 14:10:28 +0300

    bugfix

--- modules/auth/admin-auth.js
+++ modules/auth/admin-auth.js
@@ -1,5 +1,7 @@
 const HeaderAuth = require('./header-auth')
 
+const ADMINS = process.env.ADMINS.split(',')
+
 class AdminAuth extends HeaderAuth {
   async authenticate (authHeader, options) {
     if (this.allowWithoutAuth(options)) {
@@ -9,7 +11,7 @@ class AdminAuth extends HeaderAuth {
     await super.authenticate(authHeader, options)
     const user = await this.stytch.users.get({ user_id: this.userId })
     if (!user) this.raise('User not found')
-    if (!user.admin) this.raise('This action is only available to admins')
+    if (!ADMINS.includes(this.userId)) this.raise('This action is only available to admins')
   }
 }
 
