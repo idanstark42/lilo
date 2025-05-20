82f9143eb49abbbd73348e0aa41f8bffc6430ea8
Idan Stark <idanstark42@gmail.com>
2025-05-20 13:19:03 +0300

    added webhooks endpoint

--- modules/mux.js
+++ modules/mux.js
@@ -1,5 +1,6 @@
 const Mux = require('@mux/mux-node')
 const express = require('express')
+const bodyParser = require('body-parser')
 const { log } = require('./log')
 const { getAuth } = require('./auth')
 
@@ -7,6 +8,8 @@ const mux = new Mux({
   tokenId: process.env.MUX_TOKEN_ID,
   tokenSecret: process.env.MUX_TOKEN_SECRET
 })
+
+const MUX_WEBHOOK_SECRET = process.env.MUX_WEBHOOK_SECRET
 
 exports.router = () => {
   const router = express.Router()
@@ -30,7 +33,25 @@ exports.router = () => {
       log.error(err)
       res.status(err.status || 500).json({ success: false, error: err.message || 'Internal Server Error' })
     }
-})
+  })
+
+  routerpost('/webhooks', bodyParser.raw({ type: 'application/json' }), async (req, res) => {
+    try {
+      // will raise an exception if the signature is invalid
+      const isValidSignature = mux.webhooks.verifySignature(req.body, req.headers, MUX_WEBHOOK_SECRET)
+      if (!isValidSignature) {
+        log.warn('Invalid webhook request')
+        throw 'Invalid webhook request'
+      }
+      // convert the raw req.body to JSON, which is originally Buffer (raw)
+      const event = JSON.parse(req.body)
+      log.info(event)
+      
+      res.json({ received: true })
+    } catch (err) {
+      return res.status(400).send(`Webhook Error: ${err.message}`)
+    }
+  })
 
   return router
 }
